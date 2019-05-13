5ceafbdf76a1f00225608c9d2fb4ccc6ee6b0b73
JishKun <50383788+JishKun@users.noreply.github.com>
2019-05-13 16:58:49 -0500

    Funciones varias de la consola
    Cambié la extensión del archivo a ".c" y agregué la función "Sleep"

---
+++ src/Console.c
@@ -0,0 +1,21 @@
+#include "Console.hpp"
+
+void Clear() {
+    #ifdef __WIN32
+    std::system("cls");
+    #endif
+    #ifdef __linux__
+    std::system("clear");
+    #endif
+}
+
+void Sleep(int milliseconds){
+  #ifdef __WIN32
+  #include <windows.h>
+  Sleep(milliseconds);
+  #endif
+  #ifdef __linux__
+  #include <unistd.h>
+  usleep(milliseconds*1000);
+  #endif
+}
